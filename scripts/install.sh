f2974cb1fb94883669c39d6c0079d6de2f31933f
Robert Cohn <Robert.S.Cohn@intel.com>
2020-02-05 16:42:17 -0600

    add doxygen 1.8.17 to container

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
+set -e
+set -x
+
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y \
                     python3-pip \
-                    python3
+                    python3 \
+                    wget
 apt-get install -y  \
-                    doxygen \
 	            git
+wget http://doxygen.nl/files/doxygen-1.8.17.linux.bin.tar.gz
+tar zxf doxygen-1.8.17.linux.bin.tar.gz
+pushd doxygen-1.8.17
+./configure
+set +e
+make install
+set -e
+popd
 apt-get install -y \
                     latexmk \
                     texlive-latex-base \
