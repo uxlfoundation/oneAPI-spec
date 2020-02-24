ecdee3f3edfb9f06e093e568cfd1d11ec297801e
Robert Cohn <rscohn2@gmail.com>
2020-02-24 10:47:48 -0500

    put title in back in breadcrumbs

--- source/conf/common_conf.py
+++ source/conf/common_conf.py
@@ -1,13 +1,13 @@
 rst_prolog = """
-.. |ccl_full_name| replace:: oneAPI Collective Communications Library
-.. |dal_full_name| replace:: oneAPI Data Analytics Library
-.. |dnn_full_name| replace:: oneAPI Deep Neural Network Library
-.. |dpl_full_name| replace:: oneAPI DPC++ Library
-.. |dpcpp_full_name| replace:: oneAPI DPC++
-.. |l0_full_name| replace:: oneAPI Level Zero
-.. |mkl_full_name| replace:: oneAPI Math Kernel Library
-.. |tbb_full_name| replace:: oneAPI Threading Building Blocks
-.. |vpl_full_name| replace:: oneAPI Video Processing Library
+.. |ccl_full_name| replace:: Collective Communications Library
+.. |dal_full_name| replace:: Data Analytics Library
+.. |dnn_full_name| replace:: Deep Neural Network Library
+.. |dpl_full_name| replace:: DPC++ Library
+.. |dpcpp_full_name| replace:: DPC++
+.. |l0_full_name| replace:: Level Zero
+.. |mkl_full_name| replace:: Math Kernel Library
+.. |tbb_full_name| replace:: Threading Building Blocks
+.. |vpl_full_name| replace:: Video Processing Library
 """
 
 oneapi_spec_version = '0.6.0'
