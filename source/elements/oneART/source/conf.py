cb38bc68d653ccf604d15be8eb281d90045a9a5d
dmnemshi <donna.nemshick@intel.com>
2021-09-24 10:22:59 -0400

    Update conf.py

--- source/elements/oneART/source/conf.py
+++ source/elements/oneART/source/conf.py
@@ -21,7 +21,7 @@ import sys
 from os.path import join
 
 
-project = 'Advanced Ray Tracing'
+project = 'oneART'
 
 repo_root = join('..', '..', '..', '..')
 exec(open(join(repo_root, 'source', 'conf', 'common_conf.py')).read())
