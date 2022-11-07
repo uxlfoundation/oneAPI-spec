b2c951c26159b892a02e7cdea5463d5a5eb594bf
Mourad Gouicem <mourad.gouicem@intel.com>
2022-11-07 10:57:17 +0100

    oneDNN: introduce graph extension headers

--- source/elements/oneDNN/include/dnnl.hpp
+++ source/elements/oneDNN/include/dnnl.hpp
@@ -1,5 +1,5 @@
-// Copyright 2016-2020 Intel Corporation
-// SPDX-FileCopyrightText: 2016-2020 Intel Corporation
+// Copyright 2016-2022 Intel Corporation
+// SPDX-FileCopyrightText: 2016-2022 Intel Corporation
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -26,15 +26,6 @@
 
 /// oneDNN namespace
 namespace dnnl {
-
-/// @addtogroup dnnl_api_utils Utilities
-/// Utility types and definitions.
-/// @{
-
-/// The exception class.
-struct error : public std::exception {};
-
-/// @} dnnl_api_utils
 
 struct stream;
 struct memory;
@@ -146,20 +137,6 @@ enum class scratchpad_mode {
     /// as the scratchpad buffers are not used concurrently by two primitive
     /// executions.
     user,
-};
-
-/// Floating-point math mode
-enum class fpmath_mode {
-    /// Default behavior, no downconversions allowed
-    strict,
-    /// Implicit f32->bf16 conversions allowed
-    bf16,
-    /// Implicit f32->f16 conversions allowed
-    f16,
-    /// Implicit f32->tf32 conversions allowed
-    tf32,
-    /// Implicit f32->f16 or f32->bf16 conversions allowed
-    any
 };
 
 /// Propagation kind.
@@ -452,86 +429,7 @@ enum class query {
 
 /// @} dnnl_api_primitives
 
-/// @addtogroup dnnl_api_engine Engine
-///
-/// An abstraction of a computational device: a CPU, a specific GPU
-/// card in the system, etc. Most primitives are created to execute
-/// computations on one specific engine. The only exceptions are reorder
-/// primitives that transfer data between two different engines.
-///
-/// @{
 
-/// An execution engine.
-struct engine {
-    /// Kinds of engines.
-    enum class kind {
-        /// An unspecified engine
-        any,
-        /// CPU engine
-        cpu,
-        /// GPU engine
-        gpu,
-    };
-
-    /// Constructs an empty engine. An empty engine cannot be used in any
-    /// operations.
-    engine();
-
-    /// Returns the number of engines of a certain kind.
-    ///
-    /// @param akind The kind of engines to count.
-    /// @returns The number of engines of the specified kind.
-    static size_t get_count(kind akind);
-
-    /// Constructs an engine.
-    ///
-    /// @param akind The kind of engine to construct.
-    /// @param index The index of the engine. Must be less than the value
-    ///     returned by #get_count() for this particular kind of engine.
-    engine(kind akind, size_t index);
-
-    /// Returns the kind of the engine.
-    /// @returns The kind of the engine.
-    kind get_kind() const;
-};
-
-/// @} dnnl_api_engine
-
-/// @addtogroup dnnl_api_stream Stream
-///
-/// An encapsulation of execution context tied to a particular engine.
-///
-/// @{
-
-/// An execution stream.
-struct stream {
-    /// Stream flags. Can be combined using the bitwise OR operator.
-    enum class flags : unsigned {
-        /// In-order execution.
-        in_order,
-        /// Out-of-order execution.
-        out_of_order,
-        /// Default stream configuration.
-        default_flags,
-    };
-
-    /// Constructs an empty stream. An empty stream cannot be used in any
-    /// operations.
-    stream();
-
-    /// Constructs a stream for the specified engine and with behavior
-    /// controlled by the specified flags.
-    ///
-    /// @param aengine Engine to create the stream on.
-    /// @param aflags Flags controlling stream behavior.
-    stream(const engine &aengine, flags aflags = flags::default_flags);
-
-    /// Waits for all primitives executing in the stream to finish.
-    /// @returns The stream itself.
-    stream &wait();
-};
-
-/// @} dnnl_api_stream
 
 /// @addtogroup dnnl_api_memory Memory
 ///
